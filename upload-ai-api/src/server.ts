41f89877b62a1827749a4ae5849266bc109d494b
walker <sarsey.james@gmail.com>
2023-09-14 00:19:01 -0300

    Criada rota de upload do video

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,10 +1,12 @@
 import {fastify} from 'fastify'
 import { getAllPrompts } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
 
 const port = 3333
 const app = fastify()
 
 app.register(getAllPrompts)
+app.register(uploadVideoRoute)
 
 app.listen({
     port: port
