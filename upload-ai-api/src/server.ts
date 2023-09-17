c86081f157fac03ef4394706c90db3677a002e3e
walker <sarsey.james@gmail.com>
2023-09-17 14:40:48 -0300

    Adicionado fastify cors e configurado

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,10 +1,15 @@
 import {fastify} from 'fastify'
+import {fastifyCors} from '@fastify/cors'
 import { getAllPrompts } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcriptions'
 
 const port = 3333
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(getAllPrompts)
 app.register(uploadVideoRoute)
